2ddbde0cdb780189273bd61b7a1e454a9f097eb4
Charles d'Harcourt <noreply>
2015-04-29 01:06:58 +0000

    Update files

---
+++ ui/draggie.reel/draggie.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/draggie.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Draggie
+ * @extends Component
+ */
+exports.Draggie = Component.specialize(/** @lends Draggie# */ {
+    constructor: {
+        value: function Draggie() {
+            this.super();
+        }
+    }
+});
